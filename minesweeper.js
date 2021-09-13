ae616feae1e291f418bc69e7a93a81ab6e9c5430
Mikael Åberg <mikael.aberg@me.com>
2021-09-13 16:01:51 +0200

    Change tile design

--- minesweeper.js
+++ minesweeper.js
@@ -10,145 +10,199 @@ const width = 20;
 
 const numberOfBombs = 50;
 
-const failedClick = { x: -1, y: -1 };
+const shadowHeight = 7;
+
+const failedClick = {
+    x: -1,
+    y: -1
+};
 
 function setup() {
-  createCanvas(width * tileSize, height * tileSize);
-  generateBoard();
-  placeBombs();
-  setAdjecentBombNumber();
+    createCanvas(width * tileSize, height * tileSize);
+    generateBoard();
+    placeBombs();
+    setAdjacentBombNumber();
 }
 
 function generateBoard() {
-  for (let x = 0; x < width; x++) {
-    board[x] = [];
-    for (let y = 0; y < height; y++) {
-      board[x][y] = new Tile(x, y);
+    for (let x = 0; x < width; x++) {
+        board[x] = [];
+        for (let y = 0; y < height; y++) {
+            board[x][y] = new Tile(x, y);
+        }
     }
-  }
 }
 
 function placeBombs() {
-  var placedBombs = 0;
-  do {
-    var x = Math.floor(random(width));
-    var y = Math.floor(random(height));
-    if (!board[x][y].hasBomb) {
-      board[x][y].setBomb();
-      placedBombs++;
-    }
-  } while (placedBombs < numberOfBombs);
+    var placedBombs = 0;
+    do {
+        var x = Math.floor(random(width));
+        var y = Math.floor(random(height));
+        if (!board[x][y].hasBomb) {
+            board[x][y].setBomb();
+            placedBombs++;
+        }
+    } while (placedBombs < numberOfBombs);
 }
 
-function setAdjecentBombNumber() {
-  for (let x = 0; x < width; x++) {
-    for (let y = 0; y < height; y++) {
-      board[x][y].setBombNumber();
+function setAdjacentBombNumber() {
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+            board[x][y].setBombNumber();
+        }
     }
-  }
 }
 
-function drawHiddleTile(tile) {
-  fill(150);
-  rect(tile.x * tileSize, tile.y * tileSize, tileSize, tileSize);
+function drawHiddenTile(tile) {
+    fill('#c0c0c0');
+    // Draw center of tile
+    rect((tile.x * tileSize) + shadowHeight, (tile.y * tileSize) + shadowHeight, tileSize - shadowHeight * 2, tileSize - shadowHeight * 2);
+
+    // Draw edges of tile
+    strokeWeight(1);
+    fill("#f5f5f5");
+    stroke('#a6a6a6');
+    drawTileTopEdge(tile);
+    drawTileLeftEdge(tile);
+    stroke('#706f6f');
+    fill("#808080");
+    drawTileRightEdge(tile);
+    drawTileBottomEdge(tile);
+
+}
+
+function drawTileTopEdge(tile) {
+    beginShape();
+    vertex((tile.x * tileSize), (tile.y * tileSize));
+    vertex((tile.x * tileSize) + tileSize, (tile.y * tileSize));
+    vertex((tile.x * tileSize) + tileSize - shadowHeight, (tile.y * tileSize) + shadowHeight);
+    vertex((tile.x * tileSize) + shadowHeight, (tile.y * tileSize) + shadowHeight);
+    endShape(CLOSE);
+}
+
+function drawTileLeftEdge(tile) {
+    beginShape();
+    vertex((tile.x * tileSize), (tile.y * tileSize));
+    vertex((tile.x * tileSize), (tile.y * tileSize) + tileSize);
+    vertex((tile.x * tileSize) + shadowHeight, (tile.y * tileSize) + tileSize - shadowHeight);
+    vertex((tile.x * tileSize) + shadowHeight, (tile.y * tileSize) + shadowHeight);
+    endShape(CLOSE);
+}
+
+function drawTileBottomEdge(tile) {
+    beginShape();
+    vertex((tile.x * tileSize), (tile.y * tileSize) + tileSize);
+    vertex((tile.x * tileSize) + tileSize, (tile.y * tileSize) + tileSize);
+    vertex((tile.x * tileSize) + tileSize - shadowHeight, (tile.y * tileSize) + tileSize - shadowHeight);
+    vertex((tile.x * tileSize) + shadowHeight, (tile.y * tileSize) + tileSize - shadowHeight);
+    endShape(CLOSE);
+}
+
+function drawTileRightEdge(tile) {
+    beginShape();
+    vertex((tile.x * tileSize) + tileSize, (tile.y * tileSize));
+    vertex((tile.x * tileSize) + tileSize - shadowHeight, (tile.y * tileSize) + shadowHeight);
+    vertex((tile.x * tileSize) + tileSize - shadowHeight, (tile.y * tileSize) - shadowHeight + tileSize);
+    vertex((tile.x * tileSize) + tileSize, (tile.y * tileSize) + tileSize);
+    endShape(CLOSE);
 }
 
 function showBoard() {
-  for (let x = 0; x < width; x++) {
-    for (let y = 0; y < height; y++) {
-      if (board[x][y].hasBomb) {
-        board[x][y].isVisible = true;
-      }
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+            if (board[x][y].hasBomb) {
+                board[x][y].isVisible = true;
+            }
+        }
     }
-  }
 }
 
 function mouseClicked(event) {
-  event.preventDefault();
-  console.log(event);
-  var x = Math.floor(mouseX / tileSize);
-  var y = Math.floor(mouseY / tileSize);
+    event.preventDefault();
+    var x = Math.floor(mouseX / tileSize);
+    var y = Math.floor(mouseY / tileSize);
 
-  if (x >= 0 && x < width && y >= 0 && y < height && !isFlipping) {
-    if (board[x][y].hasBomb) {
-      failedClick.x = x;
-      failedClick.y = y;
-      showBoard();
-      noLoop();
-    } else {
-      board[x][y].setVisible();
-      tilesToFlip = board[x][y].getFlippableNeighbors();
-      console.log(tilesToFlip);
-      currentFlipDuration = 0;
-      isFlipping = true;
+    if (x >= 0 && x < width && y >= 0 && y < height && !isFlipping) {
+        if (board[x][y].hasBomb) {
+            failedClick.x = x;
+            failedClick.y = y;
+            showBoard();
+            noLoop();
+        } else {
+            board[x][y].setVisible();
+            tilesToFlip = board[x][y].getFlippableNeighbors();
+            currentFlipDuration = 0;
+            isFlipping = true;
+        }
     }
-  }
 }
 
 function flipTiles() {
-  let newTiles = [];
-  for (const tile of tilesToFlip) {
-    tile.setVisible();
-  }
+    let newTiles = [];
+    for (const tile of tilesToFlip) {
+        tile.setVisible();
+    }
 
-  for (const tile of tilesToFlip) {
-    // TODO - Find a better way to do this..
-    newTiles.push(...tile.getFlippableNeighbors().filter(x => newTiles.indexOf(x) === -1));
-  }
+    for (const tile of tilesToFlip) {
+        // TODO - Find a better way to do this..
+        newTiles.push(...tile.getFlippableNeighbors().filter(x => newTiles.indexOf(x) === -1));
+    }
 
-  console.log(tilesToFlip.length);
-
-  if (newTiles.length > 0) {
-    tilesToFlip = newTiles;
-    currentFlipDuration = 0;
-  } else {
-    isFlipping = false;
-  }
+    if (newTiles.length > 0) {
+        tilesToFlip = newTiles;
+        currentFlipDuration = 0;
+    } else {
+        isFlipping = false;
+    }
 }
 
 function drawBoard() {
-  for (let x = 0; x < width; x++) {
-    for (let y = 0; y < height; y++) {
-      var tile = board[x][y];
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+            var tile = board[x][y];
 
-      if (!tile.isVisible) {
-        drawHiddleTile(tile);
-      } else {
-        fill(tile.x === failedClick.x && tile.y === failedClick.y ? 0 : 255);
-        rect(tile.x * tileSize, tile.y * tileSize, tileSize, tileSize);
-        if (tile.hasBomb) {
-          fill(219, 33, 33);
-          circle(
-            tile.x * tileSize + tileSize / 2,
-            tile.y * tileSize + tileSize / 2,
-            tileSize / 2
-          );
-          continue;
+            if (!tile.isVisible) {
+                drawHiddenTile(tile);
+            } else {
+                fill(tile.x === failedClick.x && tile.y === failedClick.y ? 0 : 255);
+                strokeWeight(2);
+                stroke('#474747');
+                rect(tile.x * tileSize, tile.y * tileSize, tileSize, tileSize);
+                if (tile.hasBomb) {
+                    strokeWeight(0);
+                    fill(219, 33, 33);
+                    circle(
+                        tile.x * tileSize + tileSize / 2,
+                        tile.y * tileSize + tileSize / 2,
+                        tileSize / 2
+                    );
+                    continue;
+                }
+                if (tile.neighborsWithBombs !== 0) {
+                    strokeWeight(0);
+                    fill(0);
+                    textSize(tileSize * 0.5);
+                    textAlign(CENTER, CENTER);
+                    text(
+                        tile.neighborsWithBombs,
+                        tile.x * tileSize + tileSize / 2,
+                        tile.y * tileSize + tileSize / 2
+                    );
+                }
+            }
         }
-        if (tile.neighborsWithBombs !== 0) {
-          fill(0);
-          textSize(tileSize * 0.5);
-          textAlign(CENTER, CENTER);
-          text(
-            tile.neighborsWithBombs,
-            tile.x * tileSize + tileSize / 2,
-            tile.y * tileSize + tileSize / 2
-          );
-        }
-      }
     }
-  }
 }
 
 function draw() {
-  background(220);
+    background(100);
 
-  if (isFlipping) {
-    currentFlipDuration++;
-    if (currentFlipDuration >= FLIP_ANIMATION_DELAY) {
-      flipTiles();
+    if (isFlipping) {
+        currentFlipDuration++;
+        if (currentFlipDuration >= FLIP_ANIMATION_DELAY) {
+            flipTiles();
+        }
     }
-  }
 
-  drawBoard();
+    drawBoard();
 }
--- tile.js
+++ tile.js
@@ -1,66 +1,66 @@
 const NEIGHBOR_OPTIONS = [
-  { x: 0, y: -1 }, // UP
-  { x: 1, y: -1 }, // UP RIGHT
-  { x: 1, y: 0 }, // RIGHT
-  { x: 1, y: 1 }, // RIGHT DOWN
-  { x: 0, y: 1 }, // DOWN
-  { x: -1, y: 1 }, // LEFT DOWN
-  { x: -1, y: 0 }, // LEFT
-  { x: -1, y: -1 }, // LEFT UP
+    { x: 0, y: -1 }, // UP
+    { x: 1, y: -1 }, // UP RIGHT
+    { x: 1, y: 0 }, // RIGHT
+    { x: 1, y: 1 }, // RIGHT DOWN
+    { x: 0, y: 1 }, // DOWN
+    { x: -1, y: 1 }, // LEFT DOWN
+    { x: -1, y: 0 }, // LEFT
+    { x: -1, y: -1 }, // LEFT UP
 ];
 
 class Tile {
-  x;
-  y;
-  hasBomb = false;
-  isVisible = false;
-  neighborsWithBombs = 0;
+    x;
+    y;
+    hasBomb = false;
+    isVisible = false;
+    neighborsWithBombs = 0;
 
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-
-  setBomb() {
-    this.hasBomb = true;
-  }
-
-  setVisible() {
-    this.isVisible = true;
-  }
-
-  getFlippableNeighbors() {
-    if(this.neighborsWithBombs !== 0){
-      return [];
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
     }
 
-    const tiles = [];
-    for (const pos of this.getNeighbors()) {
-      if (!board[pos.x][pos.y].isVisible && !board[pos.x][pos.y].hasBomb) {
-        tiles.push(board[pos.x][pos.y]);
-      }
+    setBomb() {
+        this.hasBomb = true;
     }
 
-    return tiles;
-  }
-
-  getNeighbors() {
-    const n = [];
-    for (const option of NEIGHBOR_OPTIONS) {
-      const pos = { x: this.x + option.x, y: this.y + option.y };
-      if (pos.x < 0 || pos.x >= width || pos.y < 0 || pos.y >= height) {
-        continue;
-      }
-      n.push(pos);
+    setVisible() {
+        this.isVisible = true;
     }
-    return n;
-  }
 
-  setBombNumber() {
-    for (const pos of this.getNeighbors()) {
-      if (board[pos.x][pos.y].hasBomb) {
-        this.neighborsWithBombs++;
-      }
+    getFlippableNeighbors() {
+        if (this.neighborsWithBombs !== 0) {
+            return [];
+        }
+
+        const tiles = [];
+        for (const pos of this.getNeighbors()) {
+            if (!board[pos.x][pos.y].isVisible && !board[pos.x][pos.y].hasBomb) {
+                tiles.push(board[pos.x][pos.y]);
+            }
+        }
+
+        return tiles;
     }
-  }
+
+    getNeighbors() {
+        const n = [];
+        for (const option of NEIGHBOR_OPTIONS) {
+            const pos = { x: this.x + option.x, y: this.y + option.y };
+            if (pos.x < 0 || pos.x >= width || pos.y < 0 || pos.y >= height) {
+                continue;
+            }
+            n.push(pos);
+        }
+        return n;
+    }
+
+    setBombNumber() {
+        for (const pos of this.getNeighbors()) {
+            if (board[pos.x][pos.y].hasBomb) {
+                this.neighborsWithBombs++;
+            }
+        }
+    }
 }
